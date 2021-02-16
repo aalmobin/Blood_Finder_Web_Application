7d6363539d11f5f1b2fc981385ef5a4d1123e377
Mahian <mdmahian580@gmail.com>
2021-02-16 20:58:07 +0000

    index page and registration page is added

--- mysite/finder/views.py
+++ mysite/finder/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import DonerRegister
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'finder/index.html')
+
+
+def register(request):
+    form = DonerRegister()
+    if request.method == 'POST':
+        form = DonerRegister(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Welcome Your account have been registerd')
+            return redirect('index')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'finder/register.html', context)
