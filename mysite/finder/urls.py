8d2e9771719bb9684806ff6e0d93c8c19e6c7612
Mahian <mdmahian580@gmail.com>
2021-02-17 20:15:37 +0000

    authentication system login logout added

--- mysite/finder/urls.py
+++ mysite/finder/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path, include
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 
@@ -6,4 +7,6 @@ urlpatterns = [
 
     path('', views.home, name='index'),
     path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='finder/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='finder/logout.html'), name='logout'),
 ]
