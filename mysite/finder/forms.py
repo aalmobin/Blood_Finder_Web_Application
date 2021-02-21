cd79841f6c315c3aa3f60eea6f640ca94050e592
Mahian <mdmahian580@gmail.com>
2021-02-21 20:59:07 +0000

    profile update page added

--- mysite/finder/forms.py
+++ mysite/finder/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class DonerRegister(UserCreationForm):
@@ -10,3 +12,10 @@ class DonerRegister(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class ProfileUpdate(forms.ModelForm):
+    """Forms for profile Update"""
+    class Meta:
+        model = Profile
+        fields = ['full_name', 'blood_group', 'gender', 'Location', 'birthdate', 'contact_no', 'img']
--- mysite/finder/views.py
+++ mysite/finder/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import DonerRegister
+from .forms import DonerRegister, ProfileUpdate
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
@@ -26,3 +26,19 @@ def register(request):
 @login_required
 def profile(request):
     return render(request, 'finder/profile.html')
+
+
+@login_required
+def updateprofile(request):
+    if request.method == 'POST':
+        p_form = ProfileUpdate(request.POST, request.FILES, instance=request.user.profile)
+        if p_form.is_valid():
+            p_form.save()
+            messages.success(request, 'Your profile have been updated')
+            return redirect('profile')
+    else:
+        p_form = ProfileUpdate(instance=request.user.profile)
+    context = {
+        'p_form': p_form,
+    }
+    return render(request, 'finder/update_profile.html', context)
